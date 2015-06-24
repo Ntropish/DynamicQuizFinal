f33c0498df66ccd5f0cb6c6243c7baddb24c2199
NTrope <ju5t1n5t0n3@gmail.com>
2015-06-24 08:50:51 -0500

    Bumbling along with handlebars
    Starting to implement a handlebars block to display a question index

--- quiz.js
+++ quiz.js
@@ -2,10 +2,8 @@
  * Created by Justin on 6/23/2015.
  */
 
-function Quiz(questions, quizName, mainDisplay, sideBarDisplay) {
+function Quiz(questions, quizName, content) {
     this.name = quizName;
-    var mainDisp = mainDisplay;
-    var sideDisp = sideBarDisplay;
     var score = 0;
     var finalized = false;
     var currentQuestion = 0;
@@ -37,4 +35,26 @@ function Quiz(questions, quizName, mainDisplay, sideBarDisplay) {
     function getFinalized() {
 
     }
+
 }
+
+Handlebars.registerHelper('display-index', function(items, options) {
+    var out = "<h4>Questions</h4><ul style='list-style-type:none'>";
+
+    items.forEach(function(item) {
+        out = out + "<li>" + options.fn(item) + "</li>";
+
+    });
+
+    return out + "</ul>";
+});
+
+$(document).ready( function() {
+    var side = $('#sidebar-template').html();
+    var sideTemp = Handlebars.compile(side);
+    $('#sidebar').html(sideTemp( {sidebar:"Sidebar!!!", questions: [{num: "1"},{num: 2}] } ));
+
+
+    //var quiz1 = new Quiz(["age", "22,23", 0], ageQuiz, $('#content'));
+
+});
